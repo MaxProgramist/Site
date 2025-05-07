4548e32f088df0dcf784d632819948dce6dce9e9
MaxProgramist <ls555431k@gmail.com>
2025-05-07 22:04:19 +0300

    D

--- JSCode/JSCode_programmingPage.js
+++ JSCode/JSCode_programmingPage.js
@@ -5,6 +5,40 @@ const SET_OF_TASKS = localStorage.getItem("setOfTasks");
 
 let editor = window.editor;
 
+let myTasks = payload.rooms[ROOM_CODE].players[THIS_PLAYER_INDEX].tasks;
+let enemyTasks = ;
+
+const waitForEditor = setInterval(() => {
+    if (window.editor) {
+        clearInterval(waitForEditor);
+        editor = window.editor;
+        editor.layout();
+    }
+}, 10);
+
+Loop();
+
+
+async function Loop() {
+    while (true) {
+        await SomeAsyncFunction();
+        await Delay(75);
+    }
+}
+
+function Delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function SomeAsyncFunction() {
+    let payload = await LoadData();
+
+    //if (payload.roomsCodes.length < 1) window.location.href = "index.html";
+
+    let myTasks = payload.rooms[ROOM_CODE].players[THIS_PLAYER_INDEX].tasks;
+    let enemyTasks = payload.rooms[ROOM_CODE].players[payload.rooms[ROOM_CODE].players[THIS_PLAYER_INDEX].enemy].tasks;
+}
+
 function UploadSolution()
 {
     if (!editor) return;
@@ -13,10 +47,3 @@ function UploadSolution()
 
     console.log(currentNewCode)
 }
-
-const waitForEditor = setInterval(() => {
-    if (window.editor) {
-        clearInterval(waitForEditor);
-        editor = window.editor;
-    }
-}, 100);
